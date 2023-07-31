a4907626baea7d628b0eb216e10777ff46e2ac17
samyak <samyakagarwal20@gmail.com>
2023-07-31 15:14:39 +0530

    added loggers

--- src/main/java/com/yflash/tech/SampleAPI/service/impl/UserServiceImpl.java
+++ src/main/java/com/yflash/tech/SampleAPI/service/impl/UserServiceImpl.java
@@ -32,42 +32,62 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public List<UserEntity> getAllUsers() {
+        LOGGER.info("Fetching all user details");
         return userRepository.findAll();
     }
 
     @Override
     public User getUserById(Integer id) {
+        LOGGER.info("Fetching user with id {}", id);
         Optional<UserEntity> userEntity = userRepository.findById(id);
         if(userEntity.isEmpty()) {
+            LOGGER.info("No user found with id {}", id);
             throw new DetailsNotFoundException(CommonConstants.GENERIC_EXCEPTION_MESSAGE, CommonConstants.DETAILS_NOT_FOUND_ERROR_CODE, Collections.singletonList(CommonConstants.HTTP_STATUS_MESSAGE_DETAILS_NOT_FOUND));
         }
         return modelMapper.map(userEntity,User.class);
     }
     @Override
     public User addUserDetails(PostUserRequest userRequest) {
-        UserEntity userEntity = modelMapper.map(userRequest,UserEntity.class);
-        userEntity = userRepository.save(userEntity);
-        return modelMapper.map(userEntity, User.class);
+        try {
+            UserEntity userEntity = modelMapper.map(userRequest, UserEntity.class);
+            LOGGER.info("Saving the user with details - firstName : {}, \t lastName : {}", userRequest.getFirstName(), userRequest.getLastName());
+            userEntity = userRepository.save(userEntity);
+            LOGGER.info("User details saved successfully with id {}", userEntity.getId());
+            return modelMapper.map(userEntity, User.class);
+        } catch (Exception e) {
+            LOGGER.error("Error while saving the details - {}", e.getMessage());
+            throw new ProcessingException(CommonConstants.GENERIC_EXCEPTION_MESSAGE, CommonConstants.INTERNAL_SERVER_ERROR_CODE, Collections.singletonList(CommonConstants.HTTP_STATUS_MESSAGE_INTERNAL_SERVER_ERROR));
+        }
     }
     @Override
     public User updateUserDetails(PutUserRequest userRequest) {
-        if(!userRepository.existsById(userRequest.getId())) {
-            throw new ProcessingException(CommonConstants.GENERIC_EXCEPTION_MESSAGE, CommonConstants.INTERNAL_SERVER_ERROR_CODE, Collections.singletonList(CommonConstants.USER_NOT_FOUND));
+        try {
+            if (!userRepository.existsById(userRequest.getId())) {
+                throw new ProcessingException(CommonConstants.GENERIC_EXCEPTION_MESSAGE, CommonConstants.INTERNAL_SERVER_ERROR_CODE, Collections.singletonList(CommonConstants.USER_NOT_FOUND));
+            }
+            UserEntity userEntity = modelMapper.map(userRequest, UserEntity.class);
+            userEntity = userRepository.save(userEntity);
+            return modelMapper.map(userEntity, User.class);
+        } catch (Exception e) {
+            LOGGER.error("Error while saving the details - {}", e.getMessage());
+            throw new ProcessingException(CommonConstants.GENERIC_EXCEPTION_MESSAGE, CommonConstants.INTERNAL_SERVER_ERROR_CODE, Collections.singletonList(CommonConstants.HTTP_STATUS_MESSAGE_INTERNAL_SERVER_ERROR));
         }
-        UserEntity userEntity = modelMapper.map(userRequest,UserEntity.class);
-        userEntity = userRepository.save(userEntity);
-        return modelMapper.map(userEntity, User.class);
     }
     @Override
     public String deleteUserDetails(Integer id) {
-        if(!userRepository.existsById(id)) {
-            throw new DetailsNotFoundException(CommonConstants.GENERIC_EXCEPTION_MESSAGE,CommonConstants.DETAILS_NOT_FOUND_ERROR_CODE, Collections.singletonList(CommonConstants.HTTP_STATUS_MESSAGE_DETAILS_NOT_FOUND));
+        try {
+            if (!userRepository.existsById(id)) {
+                throw new DetailsNotFoundException(CommonConstants.GENERIC_EXCEPTION_MESSAGE, CommonConstants.DETAILS_NOT_FOUND_ERROR_CODE, Collections.singletonList(CommonConstants.HTTP_STATUS_MESSAGE_DETAILS_NOT_FOUND));
+            }
+            if (userRepository.existsById(id)) {
+                userRepository.deleteById(id);
+                return id.toString();
+            }
+            return null;
+        } catch (Exception e) {
+            LOGGER.error("Error while deleting the user details - {}", e.getMessage());
+            throw new ProcessingException(CommonConstants.GENERIC_EXCEPTION_MESSAGE, CommonConstants.INTERNAL_SERVER_ERROR_CODE, Collections.singletonList(CommonConstants.HTTP_STATUS_MESSAGE_INTERNAL_SERVER_ERROR));
         }
-        if(userRepository.existsById(id)) {
-            userRepository.deleteById(id);
-            return id.toString();
-        }
-        return null;
     }
 }
 
